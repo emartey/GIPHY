13bb4a7f7f3e531021670a92376f4699b00a99d0
Edward Martey <martey8709@gmail.com>
2019-10-19 05:01:41 -0500

    search images showing

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,7 @@
-$(document).ready(function () {
+$(function () {
     populateButtons(searchArray, 'searchButton', '#buttonsArea')
 
-})
+});
 
 
 var searchArray = ["Dog", "Cat", "Bird"];
@@ -15,27 +15,40 @@ function populateButtons(searchArray, classToAdd, areaToAddTo) {
         a.text(searchArray[i]);
         $(areaToAddTo).append(a);
     }
-}
+};
 
 $(document).on('click', '.searchButton', function () {
     $('#searches').empty();
     var type = $(this).data('type');
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        type + "&api_key=4FjTj3ftqhkLlx64m54UyQtsPQUbdiXN";
-    ;
+        type + "&api_key=4FjTj3ftqhkLlx64m54UyQtsPQUbdiXN&limit=10";
+
     $.ajax({
-        method: 'GET',
-        URL: queryURL
-    }
-        .done(function (response) {
-            var results = response.data;
+        url: queryURL,
+        method: 'GET'
+    }).done(function (response) {
+        console.log(response)
+        for (var i = 0; i < response.data.length; i++) {
 
-            for (var i = 0; i < response.length; i++) {
-                var searchDiv = $('<div class= "search-item">');
-            }
+            var searchDiv = $('<div class= "search-item">');
+            var rating = response.data[i].rating;
+            var p = $('<p>').text('Rating: ' + rating);
+            var animated = response.data[i].images.fixed_height.url;
+            var still = response.data[i].images.fixed_height_still.url;
+            var image = $('<img>');
+            image.attr('src', still);
+            image.attr('data-still', still);
+            image.attr('data-animated', animated);
+            image.attr('data-state', 'still');
+            image.addClass('searchImage');
+            searchDiv.append(p);
+            searchDiv.append(image);
+            $('#searches').append(searchDiv);
+        }
 
-        })
-    )
+
+    })
+
 
 })
 
